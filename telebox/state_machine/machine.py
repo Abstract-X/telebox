f8f11526da73438dcf78d829f618b625911441b9
Abstract-X <abstract-x-mail@protonmail.com>
2022-10-16 09:47:48 +0300

    Add `StateMachine.add_state`

--- telebox/state_machine/machine.py
+++ telebox/state_machine/machine.py
@@ -1,4 +1,5 @@
 from typing import Optional, Any
+import contextlib
 
 from telebox.state_machine.state import State
 from telebox.state_machine.storages.storage import AbstractStateStorage
@@ -8,7 +9,8 @@ from telebox.state_machine.magazine import StateMagazine
 from telebox.state_machine.errors import (
     DestinationStateNotFoundError,
     NextStateNotFoundError,
-    PreviousStateNotFoundError
+    PreviousStateNotFoundError,
+    StateExistsError
 )
 from telebox.dispatcher.handlers.handlers.event import AbstractEventHandler
 from telebox.typing import Event
@@ -28,6 +30,9 @@ class StateMachine:
     def states(self) -> set[State]:
         return self._state_manager.states
 
+    def add_state(self, state: State) -> None:
+        self._state_manager.add_state(state)
+
     def add_transition(
         self,
         source_state: State,
@@ -43,8 +48,8 @@ class StateMachine:
         )
 
         for i in (source_state, destination_state):
-            if i not in self._state_manager.states:
-                self._state_manager.add_state(i)
+            with contextlib.suppress(StateExistsError):
+                self.add_state(i)
 
     def get_state(self, *, chat_id: int, user_id: Optional[int] = None) -> State:
         magazine = self._state_manager.load_magazine(chat_id=chat_id, user_id=user_id)
