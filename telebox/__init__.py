003130c3b2f38f1f40ab98d9629aaa73ae2b602e
Abstract-X <abstract-x-mail@protonmail.com>
2022-10-23 02:47:13 +0300

    Add `MediaGroupContentType` to `__all__`

--- telebox/__init__.py
+++ telebox/__init__.py
@@ -39,6 +39,7 @@ __all__ = [
     "EventHandler",
     "ErrorHandler",
     "EventType",
+    "MediaGroupContentType",
     "AbstractHandler",
     "AbstractEventHandler",
     "AbstractErrorHandler",
