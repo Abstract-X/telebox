3ffe385b620dff975a5705f69c53c43c6495c6d9
Abstract-X <abstract-x-mail@protonmail.com>
2022-10-29 01:21:25 +0300

    Make `RateLimiter.limit` public

--- telebox/dispatcher/utils/rate_limiter/rate_limiter.py
+++ telebox/dispatcher/utils/rate_limiter/rate_limiter.py
@@ -7,7 +7,7 @@ from telebox.dispatcher.utils.rate_limiter.call_tracker import CallTracker
 class RateLimiter:
 
     def __init__(self, limit: RateLimit):
-        self._limit = limit
+        self.limit = limit
         self._call_tracker = CallTracker(
             chat_ttl_secs=limit.chat_limit_secs,
             user_ttl_secs=limit.user_limit_secs,
@@ -22,40 +22,40 @@ class RateLimiter:
         chat_id_is_not_none = chat_id is not None
         user_id_is_not_none = user_id is not None
 
-        if chat_id_is_not_none and self._limit.check_chat_limit():
+        if chat_id_is_not_none and self.limit.check_chat_limit():
             chat_calls = self._call_tracker.get_chat_calls(chat_id)
 
-            if chat_calls < self._limit.chat_limit_calls:
+            if chat_calls < self.limit.chat_limit_calls:
                 self._processed_chat_ids.discard(chat_id)
-            elif chat_calls == self._limit.chat_limit_calls:
+            elif chat_calls == self.limit.chat_limit_calls:
                 if chat_id not in self._processed_chat_ids:
                     self._processed_chat_ids.add(chat_id)
-                    self._limit.process_chat_over_limit(chat_id)
+                    self.limit.process_chat_over_limit(chat_id)
 
                 is_limited = True
 
-        if user_id_is_not_none and self._limit.check_user_limit():
+        if user_id_is_not_none and self.limit.check_user_limit():
             user_calls = self._call_tracker.get_user_calls(user_id)
 
-            if user_calls < self._limit.user_limit_calls:
+            if user_calls < self.limit.user_limit_calls:
                 self._processed_user_ids.discard(user_id)
-            elif user_calls == self._limit.user_limit_calls:
+            elif user_calls == self.limit.user_limit_calls:
                 if user_id not in self._processed_user_ids:
                     self._processed_user_ids.add(user_id)
-                    self._limit.process_user_over_limit(user_id)
+                    self.limit.process_user_over_limit(user_id)
 
                 is_limited = True
 
-        if chat_id_is_not_none and user_id_is_not_none and self._limit.check_chat_user_limit():
+        if chat_id_is_not_none and user_id_is_not_none and self.limit.check_chat_user_limit():
             chat_user_calls = self._call_tracker.get_chat_user_calls(chat_id, user_id)
             chat_user_ids = (chat_id, user_id)
 
-            if chat_user_calls < self._limit.chat_user_limit_calls:
+            if chat_user_calls < self.limit.chat_user_limit_calls:
                 self._process_chat_user_ids.discard(chat_user_ids)
-            elif chat_user_calls == self._limit.chat_user_limit_calls:
+            elif chat_user_calls == self.limit.chat_user_limit_calls:
                 if (chat_id, user_id) not in self._process_chat_user_ids:
                     self._process_chat_user_ids.add((chat_id, user_id))
-                    self._limit.process_chat_user_over_limit(chat_id, user_id)
+                    self.limit.process_chat_user_over_limit(chat_id, user_id)
 
                 is_limited = True
 
