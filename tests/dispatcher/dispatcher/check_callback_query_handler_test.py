85a6fb3b6371580a3a44460d63900d5cfbb6260a
Abstract-X <abstract-x-mail@protonmail.com>
2022-10-29 20:18:47 +0300

    Add tests for `Dispatcher.check_callback_query_handler`

---
+++ tests/dispatcher/dispatcher/check_callback_query_handler_test.py
@@ -0,0 +1,67 @@
+from unittest.mock import Mock
+from typing import Optional, Union
+
+import pytest
+
+from telebox import (
+    Dispatcher,
+    TelegramBot,
+    AbstractEventBaseFilter,
+    RateLimit,
+    Limit,
+    NotSet,
+    NOT_SET
+)
+from tests.dispatcher.dispatcher.helpers import CallbackQueryHandler, CallbackQueryFilter
+
+
+TEST_FILTER_DATA = (
+    ("filter_",),
+    (
+        (None,),
+        (CallbackQueryFilter(),),
+        (~CallbackQueryFilter(),),
+        (CallbackQueryFilter() & CallbackQueryFilter(),),
+        (CallbackQueryFilter() | CallbackQueryFilter(),)
+    )
+)
+TEST_RATE_LIMIT_DATA = (
+    ("rate_limit",),
+    (
+        (NOT_SET,),
+        (None,),
+        (RateLimit(user_limit=Limit(1, 1)),)
+    )
+)
+
+
+@pytest.mark.parametrize(*TEST_FILTER_DATA)
+@pytest.mark.parametrize(*TEST_RATE_LIMIT_DATA)
+def test(
+    filter_: Optional[AbstractEventBaseFilter],
+    rate_limit: Union[RateLimit, None, NotSet],
+    token: str
+) -> None:
+    bot = TelegramBot(session=Mock(), token=token)
+    dispatcher = Dispatcher(bot)
+    handler = CallbackQueryHandler()
+
+    assert dispatcher.check_callback_query_handler(handler, filter_, rate_limit) is False
+
+    dispatcher.add_callback_query_handler(handler, filter_, rate_limit)
+
+    assert dispatcher.check_callback_query_handler(handler, filter_, rate_limit) is True
+
+
+def test_default_rate_limit(token: str) -> None:
+    bot = TelegramBot(session=Mock(), token=token)
+    rate_limit = RateLimit(user_limit=Limit(1, 1))
+    dispatcher = Dispatcher(bot, default_rate_limit=rate_limit)
+    handler = CallbackQueryHandler()
+    filter_ = CallbackQueryFilter()
+
+    assert dispatcher.check_callback_query_handler(handler, filter_, rate_limit) is False
+
+    dispatcher.add_callback_query_handler(handler, filter_)
+
+    assert dispatcher.check_callback_query_handler(handler, filter_, rate_limit) is True
