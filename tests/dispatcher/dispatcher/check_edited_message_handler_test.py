c3bd5a63e56d401b9fae70fd499a75f51090086a
Abstract-X <abstract-x-mail@protonmail.com>
2022-10-29 19:40:42 +0300

    Add tests for `Dispatcher.check_edited_message_handler`

---
+++ tests/dispatcher/dispatcher/check_edited_message_handler_test.py
@@ -0,0 +1,67 @@
+from unittest.mock import Mock
+from typing import Optional, Union
+
+import pytest
+
+from telebox import (
+    Dispatcher,
+    TelegramBot,
+    AbstractEventBaseFilter,
+    RateLimit,
+    Limit,
+    NotSet,
+    NOT_SET
+)
+from tests.dispatcher.dispatcher.helpers import EditedMessageHandler, EditedMessageFilter
+
+
+TEST_FILTER_DATA = (
+    ("filter_",),
+    (
+        (None,),
+        (EditedMessageFilter(),),
+        (~EditedMessageFilter(),),
+        (EditedMessageFilter() & EditedMessageFilter(),),
+        (EditedMessageFilter() | EditedMessageFilter(),)
+    )
+)
+TEST_RATE_LIMIT_DATA = (
+    ("rate_limit",),
+    (
+        (NOT_SET,),
+        (None,),
+        (RateLimit(chat_limit=Limit(1, 1)),)
+    )
+)
+
+
+@pytest.mark.parametrize(*TEST_FILTER_DATA)
+@pytest.mark.parametrize(*TEST_RATE_LIMIT_DATA)
+def test(
+    filter_: Optional[AbstractEventBaseFilter],
+    rate_limit: Union[RateLimit, None, NotSet],
+    token: str
+) -> None:
+    bot = TelegramBot(session=Mock(), token=token)
+    dispatcher = Dispatcher(bot)
+    handler = EditedMessageHandler()
+
+    assert dispatcher.check_edited_message_handler(handler, filter_, rate_limit) is False
+
+    dispatcher.add_edited_message_handler(handler, filter_, rate_limit)
+
+    assert dispatcher.check_edited_message_handler(handler, filter_, rate_limit) is True
+
+
+def test_default_rate_limit(token: str) -> None:
+    bot = TelegramBot(session=Mock(), token=token)
+    rate_limit = RateLimit(chat_limit=Limit(1, 1))
+    dispatcher = Dispatcher(bot, default_rate_limit=rate_limit)
+    handler = EditedMessageHandler()
+    filter_ = EditedMessageFilter()
+
+    assert dispatcher.check_edited_message_handler(handler, filter_, rate_limit) is False
+
+    dispatcher.add_edited_message_handler(handler, filter_)
+
+    assert dispatcher.check_edited_message_handler(handler, filter_, rate_limit) is True
