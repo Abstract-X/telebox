5dbf66a304c213d80ea9245ce9edcd0179a2a8ef
Abstract-X <abstract-x-mail@protonmail.com>
2022-10-29 23:34:48 +0300

    Add tests for `StateMachine`

--- tests/state_machine/state_machine/add_state_test.py
+++ tests/state_machine/state_machine/add_state_test.py
@@ -1,5 +1,8 @@
+import pytest
+
 from telebox import StateMachine
 from telebox.state_machine.storages import MemoryStateStorage
+from telebox.state_machine.errors import StateNameExistsError, StateExistsError
 from tests.state_machine.state_machine.helpers import InitialState, FooState
 
 
@@ -9,3 +12,24 @@ def test() -> None:
     machine.add_state(foo_state)
 
     assert machine.check_state(foo_state)
+
+
+def test_existing_state() -> None:
+    machine = StateMachine(InitialState(), MemoryStateStorage())
+    foo_state = FooState()
+    machine.add_state(foo_state)
+
+    with pytest.raises(StateExistsError):
+        machine.add_state(foo_state)
+
+
+def test_state_with_existing_name() -> None:
+    machine = StateMachine(InitialState(), MemoryStateStorage())
+    machine.add_state(
+        FooState()
+    )
+
+    with pytest.raises(StateNameExistsError):
+        machine.add_state(
+            FooState()
+        )
--- tests/state_machine/state_machine/add_transition_test.py
+++ tests/state_machine/state_machine/add_transition_test.py
@@ -4,6 +4,7 @@ import pytest
 
 from telebox import StateMachine
 from telebox.state_machine.storages import MemoryStateStorage
+from telebox.state_machine.errors import TransitionExistsError
 from tests.state_machine.state_machine.helpers import InitialState, FooState, Handler
 
 
@@ -25,3 +26,15 @@ def test(direction: Optional[str]) -> None:
     machine.add_transition(initial_state, foo_state, handler, direction)
 
     assert machine.check_transition(initial_state, foo_state, handler, direction)
+
+
+@pytest.mark.parametrize(*TEST_DIRECTION_DATA)
+def test_existing_transition(direction: Optional[str]) -> None:
+    initial_state = InitialState()
+    machine = StateMachine(initial_state, MemoryStateStorage())
+    foo_state = FooState()
+    handler = Handler()
+    machine.add_transition(initial_state, foo_state, handler, direction)
+
+    with pytest.raises(TransitionExistsError):
+        machine.add_transition(initial_state, foo_state, handler, direction)
