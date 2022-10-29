9fe918c44436349af87a7d792129afdde6cac878
Abstract-X <abstract-x-mail@protonmail.com>
2022-10-29 18:14:26 +0300

    Add tests for `Dispatcher.add_middleware`

---
+++ tests/dispatcher/dispatcher/add_middleware_test.py
@@ -0,0 +1,12 @@
+from unittest.mock import Mock
+
+from telebox import Dispatcher, TelegramBot, Middleware
+
+
+def test(token: str) -> None:
+    bot = TelegramBot(session=Mock(), token=token)
+    dispatcher = Dispatcher(bot)
+    middleware = Middleware()
+    dispatcher.add_middleware(middleware)
+
+    assert dispatcher.check_middleware(middleware)
