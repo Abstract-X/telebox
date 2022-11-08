9aadd50c3394d68995bb5c8ddfc1948b090de143
Abstract-X <abstract-x-mail@protonmail.com>
2022-11-08 03:25:39 +0300

    Remove `event` name from `ContextStateMachine` methods

--- telebox/context/state_machine.py
+++ telebox/context/state_machine.py
@@ -21,9 +21,8 @@ class ContextStateMachine:
         *,
         for_chat: bool = False
     ) -> None:
-        event = event_context.get()
         self._machine.set_next_state(
-            event=event,
+            event=event_context.get(),
             handler=event_handler_context.get(),
             direction=direction,
             data=data,
@@ -32,19 +31,17 @@ class ContextStateMachine:
         )
 
     def set_previous_state(self, data: Any = None, *, for_chat: bool = False) -> None:
-        event = event_context.get()
         self._machine.set_previous_state(
-            event=event,
+            event=event_context.get(),
             data=data,
             chat_id=get_event_chat_id(),
             user_id=None if for_chat else get_event_user_id()
         )
 
     def set_state(self, state: State, data: Any = None, *, for_chat: bool = False) -> None:
-        event = event_context.get()
         self._machine.set_state(
             state=state,
-            event=event,
+            event=event_context.get(),
             data=data,
             chat_id=get_event_chat_id(),
             user_id=None if for_chat else get_event_user_id()
@@ -57,9 +54,8 @@ class ContextStateMachine:
         with_exit: bool = True,
         for_chat: bool = False
     ) -> None:
-        event = event_context.get()
         self._machine.reset_state(
-            event=event,
+            event=event_context.get(),
             data=data,
             chat_id=get_event_chat_id(),
             user_id=None if for_chat else get_event_user_id(),
