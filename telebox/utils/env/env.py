02e8f6201469dffc47619cb9e8d1fdb35b7f92b3
Abstract-X <abstract-x-mail@protonmail.com>
2022-11-09 06:46:35 +0300

    Add `Env.initialize`, remove `Env.check_vars`

--- telebox/utils/env/env.py
+++ telebox/utils/env/env.py
@@ -6,7 +6,13 @@ from telebox.utils.env.errors import EnvError
 @dataclass
 class Env:
 
-    def check_vars(self) -> None:
+    def initialize(self, **kwargs) -> None:
+        for name, value in kwargs.items():
+            if name not in vars(self):
+                raise EnvError(f"No {name!r} field!")
+
+            setattr(self, name, value)
+
         for name, value in vars(self).items():
             if value is None:
                 raise EnvError(f"{name!r} is not set!")
