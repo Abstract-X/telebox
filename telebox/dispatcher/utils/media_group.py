77ee81dc019c44e35d9c7dcbb3ae6221864b6e55
Abstract-X <abstract-x-mail@protonmail.com>
2022-12-19 05:58:56 +0300

    Add `message_id` property to `MediaGroup`

--- telebox/dispatcher/utils/media_group.py
+++ telebox/dispatcher/utils/media_group.py
@@ -30,6 +30,10 @@ class MediaGroup:
         return self.messages[0].chat
 
     @property
+    def message_id(self) -> int:
+        return self.messages[0].message_id
+
+    @property
     def message_thread_id(self) -> Optional[int]:
         return self.messages[0].message_thread_id
 
