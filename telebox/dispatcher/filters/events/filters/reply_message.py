83644fc8751886bd0a73b6a45386e93ef5652704
Abstract-X <abstract-x-mail@protonmail.com>
2023-02-14 16:28:37 +0300

    Add `user_ids` parameter to `ReplyMessageFilter`

--- telebox/dispatcher/filters/events/filters/reply_message.py
+++ telebox/dispatcher/filters/events/filters/reply_message.py
@@ -8,6 +8,9 @@ from telebox.bot.types.types.message import Message
 
 class ReplyMessageFilter(AbstractEventFilter):
 
+    def __init__(self, *user_ids: int):
+        self._user_ids = set(user_ids)
+
     def get_event_types(self) -> set[EventType]:
         return {
             EventType.MESSAGE,
@@ -19,4 +22,10 @@ class ReplyMessageFilter(AbstractEventFilter):
         }
 
     def check_event(self, event: Union[Message, MediaGroup]) -> bool:
-        return event.is_reply
+        if event.is_reply:
+            if self._user_ids:
+                return event.reply_to_message.user_id in self._user_ids
+
+            return True
+
+        return False
