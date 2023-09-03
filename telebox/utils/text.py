086ac5c7ea4ca9e56bbbe9a8057f8f30b9ce0849
Abstract-X <abstract-x-mail@protonmail.com>
2023-09-03 19:38:47 +0300

    Fix text formatting

--- telebox/utils/text.py
+++ telebox/utils/text.py
@@ -4,14 +4,28 @@ from telebox.bot.utils.formatting import get_escaped_html_text, get_escaped_mark
 
 
 def get_html_text(template: str, /, *args, **kwargs) -> str:
-    return _get_text(get_escaped_html_text, template, *args, **kwargs)
+    return _get_formatted_text(get_escaped_html_text, template, *args, **kwargs)
 
 
 def get_markdown_text(template: str, /, *args, **kwargs) -> str:
-    return _get_text(get_escaped_markdown_text, template, *args, **kwargs)
+    return _get_formatted_text(get_escaped_markdown_text, template, *args, **kwargs)
 
 
-def _get_text(escaping_func: Callable[[str], str], template: str, /, *args, **kwargs) -> str:
+def get_text_with_surrogates(text: str) -> bytes:
+    return text.encode("UTF-16-LE")
+
+
+def get_text_without_surrogates(text: bytes) -> str:
+    return text.decode("UTF-16-LE")
+
+
+def _get_formatted_text(
+    escaping_func: Callable[[str], str],
+    template: str,
+    /,
+    *args,
+    **kwargs
+) -> str:
     args = tuple(escaping_func(str(i)) for i in args)
     kwargs = {
         name: escaping_func(str(value))
