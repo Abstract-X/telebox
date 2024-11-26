a1f6f6019085851c73054b9d257897fdbea67c57
Abstract-X <abstract-x-mail@protonmail.com>
2024-11-27 02:07:52 +0300

    Improve signal processing

--- telebox/utils/signals.py
+++ telebox/utils/signals.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 import logging
+from threading import Event
 from signal import signal, Signals, SIGINT, SIGTERM
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Iterable
+import sys
 
 if TYPE_CHECKING:
     from telebox.dispatcher.dispatcher import Dispatcher
@@ -10,15 +12,26 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-def set_signal_handler(dispatcher: Dispatcher) -> None:
+def set_signal_handler(
+    dispatcher: Dispatcher,
+    *,
+    signals: Iterable[int] = (SIGINT, SIGTERM)
+) -> Event:
+    stop_event = Event()
+
     def process_shutdown(signal_number: int, _) -> None:
         signal_name = Signals(signal_number).name
         logger.info("Signal handler called with signal %r (%r).", signal_name, signal_number)
+        stop_event.set()
 
         if dispatcher.polling_is_used:
             dispatcher.stop_polling()
         elif dispatcher.server_is_used:
             dispatcher.stop_server()
+        else:
+            sys.exit()
 
-    for i in (SIGINT, SIGTERM):
+    for i in signals:
         signal(i, process_shutdown)
+
+    return stop_event
