db45887458f322b3615420ee5c0785aa9feaba0d
Abstract-X <abstract-x-mail@protonmail.com>
2024-12-24 04:13:58 +0300

    Rename `AbstractCallbackDataBuilder` methods

--- telebox/utils/callback_data_builders/builder.py
+++ telebox/utils/callback_data_builders/builder.py
@@ -4,19 +4,19 @@ from abc import ABC, abstractmethod
 class AbstractCallbackDataBuilder(ABC):
 
     @abstractmethod
-    def get_string(self, key, value=None) -> str:
+    def get_data(self, key, value=None) -> str:
         pass
 
     @abstractmethod
-    def get_data(self, string: str):
+    def get_parsed_data(self, string: str):
         pass
 
     def get_key(self, string: str):
-        key, _ = self.get_data(string)
+        key, _ = self.get_parsed_data(string)
 
         return key
 
     def get_value(self, string: str):
-        _, value = self.get_data(string)
+        _, value = self.get_parsed_data(string)
 
         return value
