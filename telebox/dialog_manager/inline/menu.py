561ea8a1052d3f77d30e26116ba8de4f3ebd1293
Abstract-X <abstract-x-mail@protonmail.com>
2025-11-15 02:41:59 +0300

    Add media support to `DialogManager`

--- telebox/dialog_manager/inline/menu.py
+++ telebox/dialog_manager/inline/menu.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Union, Optional
 
 from telebox.dispatcher.router import Router
 from telebox.dialog_manager.menu import AbstractMenu
 from telebox.dialog_manager.inline.keyboard import InlineKeyboard
+from telebox.bot.types.input_media import InputMedia
+from telebox.bot.types.message_entity import MessageEntity
 from telebox.utils.unset import Unset, UNSET
 
 
@@ -18,11 +20,15 @@ class InlineMenu(AbstractInlineMenu):
         self,
         text: str,
         keyboard: InlineKeyboard,
-        parse_mode: Union[str, None, Unset] = UNSET
+        parse_mode: Union[str, None, Unset] = UNSET,
+        entities: Union[list[MessageEntity], None, Unset] = UNSET,
+        media: Optional[InputMedia] = None
     ):
         self.text = text
         self.keyboard = keyboard
         self.parse_mode = parse_mode
+        self.entities = entities
+        self.media = media
 
     def get_text(self, deps) -> str:
         return self.text
@@ -30,8 +36,14 @@ class InlineMenu(AbstractInlineMenu):
     def get_keyboard(self, deps) -> InlineKeyboard:
         return self.keyboard
 
+    def get_media(self, deps) -> Optional[InputMedia]:
+        return self.media
+
     def set_handlers(self, router: Router) -> None:
         self.keyboard.set_handlers(router=router)
 
     def get_parse_mode(self) -> Union[str, None, Unset]:
         return self.parse_mode
+
+    def get_entities(self) -> Union[list[MessageEntity], None, Unset]:
+        return self.entities
--- telebox/dialog_manager/reply/menu.py
+++ telebox/dialog_manager/reply/menu.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Union, Optional
 
 from telebox.dialog_manager.menu import AbstractMenu
 from telebox.dialog_manager.reply.keyboard import ReplyKeyboard
+from telebox.bot.types.input_media import InputMedia
+from telebox.bot.types.message_entity import MessageEntity
 from telebox.dispatcher.router import Router
 from telebox.utils.unset import Unset, UNSET
 
@@ -18,11 +20,15 @@ class ReplyMenu(AbstractReplyMenu):
         self,
         text: str,
         keyboard: ReplyKeyboard,
-        parse_mode: Union[str, None, Unset] = UNSET
+        parse_mode: Union[str, None, Unset] = UNSET,
+        entities: Union[list[MessageEntity], None, Unset] = UNSET,
+        media: Optional[InputMedia] = None
     ):
         self.text = text
         self.keyboard = keyboard
         self.parse_mode = parse_mode
+        self.entities = entities
+        self.media = media
 
     def get_text(self, deps) -> str:
         return self.text
@@ -30,8 +36,14 @@ class ReplyMenu(AbstractReplyMenu):
     def get_keyboard(self, deps) -> ReplyKeyboard:
         return self.keyboard
 
+    def get_media(self, deps) -> Optional[InputMedia]:
+        return self.media
+
     def set_handlers(self, router: Router) -> None:
         self.keyboard.set_handlers(router=router)
 
     def get_parse_mode(self) -> Union[str, None, Unset]:
         return self.parse_mode
+
+    def get_entities(self) -> Union[list[MessageEntity], None, Unset]:
+        return self.entities
